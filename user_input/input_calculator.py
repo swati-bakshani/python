928f820433edf376ecd64fe773d824f03b6e3234
swati <swati.bakshani@gmail.com>
2023-05-17 22:42:24 +0530

    input function manually

---
+++ user_input/input_calculator.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    print(f"Addition for {a} and {b}: ")
+    print(a+b)
+
+first_value = int(input("enter first value: "))
+
+# print(type(first_value))
+
+second_value = int(input("enter second value:"))
+add(first_value,second_value)
