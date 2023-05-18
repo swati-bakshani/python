c88a65f5d41e54495a4a84e97f712d47a3dc80c1
swati <swati.bakshani@gmail.com>
2023-05-19 00:07:48 +0530

    while loop including break and continue

---
+++ loop/while_loop.py
@@ -0,0 +1,26 @@
+# https://www.w3schools.com/python/python_while_loops.asp
+
+# i= 1
+# while i < 6:
+#     print(i)
+#     i = i + 1
+#
+# print("-------------------------------------")
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if (i == 3):
+#         break
+#     i = i+1
+
+# print("-------------------------------------")
+#
+i = 0
+while i < 6:
+    print("value of i:"+ str(i))
+    i = i+1
+    if i == 3:
+        continue
+    print(i)
+
