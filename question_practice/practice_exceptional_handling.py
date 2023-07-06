695a84632481758fec4b1afef8aff96cac81d890
swati <swati.bakshani@gmail.com>
2023-07-06 10:16:50 +0530

    practice exception handling

---
+++ question_practice/practice_exceptional_handling.py
@@ -0,0 +1,8 @@
+x = 2
+
+print(type(x))
+
+if x<0:
+    raise Exception("sorry no values less than zero")
+else:
+    print("welcome")
---
+++ question_practice/practice_try_except.py
@@ -0,0 +1,15 @@
+try:
+    x = 1
+    print(x)
+    print(y)
+
+except:
+    print("something went wrong")
+
+finally:
+    print("remove your card")
+
+# x= "hello"
+# print(type(x))
+# if not type(x) is int:
+#     raise TypeError("only integers are allowed")
