cb9c1a3401f3bd1653a7f83e5feadd5b5b8399a0
swati <swati.bakshani@gmail.com>
2023-08-25 23:22:02 +0530

    my linked list

---
+++ data_structure_algo/linked_list_append.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+my_linked_list = LinkedList(4)
+print(my_linked_list.head.value)
+
+    # def append(self,value):
+    # def prepend(self,value):
+    # def insert(self,value):
+
+def print_list(self):
+    temp = self.value
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
--- dictionaries/list_in_dictionaries.py
+++ dictionaries/list_in_dictionaries.py
@@ -12,7 +12,17 @@ thisdict={
 # print(thisdict.values())
 # print(thisdict["colours[0]"])
 
-for key,value in thisdict.items():
-    # print(key, value)
-    if key == 'colours':
-        print(value[0])
+# for key,value in thisdict.items():
+#     # print(key, value)
+#     if key == 'colours':
+#         print(value[0])
+
+numbers = [3,6,2,4,3,6,8,9]
+print(len(numbers))
+for i in range(len(numbers)):
+    print(f"value of i: {i}")
+    for j in range(i+1, len(numbers)):
+        print(f"value of j: {j}")
+        if numbers[i] == numbers[j]:
+            print(str(numbers[i]) + " is a duplicate")
+            break
