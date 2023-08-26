607aa7117fae5b0df16f744f404d87bafb11dc49
swati <swati.bakshani@gmail.com>
2023-08-26 11:20:18 +0530

    append linked list

--- data_structure_algo/linked_list_append.py
+++ data_structure_algo/linked_list_append.py
@@ -9,15 +9,32 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-my_linked_list = LinkedList(4)
-print(my_linked_list.head.value)
+
+# my_linked_list = LinkedList(4)
+# print(my_linked_list.head.value)
 
     # def append(self,value):
     # def prepend(self,value):
     # def insert(self,value):
 
-def print_list(self):
-    temp = self.value
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(3)
+my_linked_list.append(6)
+
+my_linked_list.print_list()
