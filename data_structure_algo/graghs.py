cfc9690b23006676e04eea7a4f3389919500198c
swati <swati.bakshani@gmail.com>
2023-09-15 23:48:55 +0530

    graph add vertex and add edge

--- data_structure_algo/graghs.py
+++ data_structure_algo/graghs.py
@@ -11,25 +11,17 @@ class Graph:
             return True
         return False
 
-my_graph = Graph()
-
-my_graph.add_vertex('A')
-
-my_graph.print_graph()
-
-class Graph:
-    def __init__(self):
-        self.adj_list = {}
-    def print_graph(self):
-        for vertex in self.adj_list:
-            print(vertex, ':', self.adj_list[vertex])
-    def add_vertex(self,vertex):
-        if vertex not in self.adj_list.keys():
-            self.adj_list[vertex] = []
+    def add_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
             return True
         return False
 
 my_graph = Graph()
 my_graph.add_vertex('A')
-my_graph.print_graph
+my_graph.add_vertex('B')
+my_graph.add_edge('A','B')
+
+my_graph.print_graph()
 
