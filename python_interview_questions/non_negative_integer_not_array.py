c53db7f4a8cdcb4fc3c213d64e614516baa8f6b1
swati <swati.bakshani@gmail.com>
2023-11-15 09:53:35 +0530

    revision

--- python_interview_questions/non_negative_integer_not_array.py
+++ python_interview_questions/non_negative_integer_not_array.py
@@ -1,3 +1,21 @@
 # given an array arr of n unique non negative integers , how can you find a non- negative integer that is not in the array?
 
-l=[0,1,2,3,4,5,7,8,9]
+# l=[0,1,2,3,4,5,7,8,9]
+
+# l=[0,1,6,2,7,5,4]
+#
+l=[3,1,6,2,7,5,4]
+
+def find_missing_number(l):
+    list_dict={}
+    for i in l:
+        list_dict[i]=True
+
+    for i in range(len(l)):
+    # for i in l:
+    #     print(i,list_dict.get(i))
+        if not list_dict.get(i):
+            return i
+    return None
+
+print(find_missing_number(l))
