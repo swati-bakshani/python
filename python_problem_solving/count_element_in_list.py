ea29f1a213bd543e8a07a34ea324c956d2585353
swati <swati.bakshani@gmail.com>
2023-11-25 11:00:04 +0530

    count o elements in a list using recursion

---
+++ python_problem_solving/count_element_in_list.py
@@ -0,0 +1,17 @@
+list = [1,2,3,4,5,6]
+
+# def count_len_list(list):
+#     print(len(list))
+
+# count_len_list(list)
+
+"""
+Using Recursive
+"""
+def count_len_list(list):
+    if not list:
+        return 0
+    return 1+count_len_list(list[1:])
+
+print(count_len_list(list))
+
