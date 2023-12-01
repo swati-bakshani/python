49b7ce9d09ca3708486458aac3e4f591631e513d
swati <swati.bakshani@gmail.com>
2023-12-01 09:53:45 +0530

    check list sorted or not revision

--- python_problem_solving/check_if_list_sorted_or_not.py
+++ python_problem_solving/check_if_list_sorted_or_not.py
@@ -1,7 +1,9 @@
 test_1 = [1,2,3,4,5]
 test_2 = [1,2,3,3,4,4,5]
 test_3 = [1,2,4,5,3,6,5]
-
+"""
+output : True or False
+"""
 def sorted_list(input_list):
     k=0
     for num in range(len(input_list)-1):
