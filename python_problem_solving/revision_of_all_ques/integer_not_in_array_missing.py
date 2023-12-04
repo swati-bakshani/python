1ef78cd795defb46b79e272fa4dd9c71c96a7581
swati <swati.bakshani@gmail.com>
2023-12-04 23:58:25 +0530

    non negative integer missing in given array

---
+++ python_problem_solving/revision_of_all_ques/integer_not_in_array_missing.py
@@ -0,0 +1,32 @@
+l = [1,2,3,4,6,7,8,9]
+
+def missing_no(l):
+    n = len(l)+1
+    expected_sum = n * (n+1) / 2
+    actual_sum = sum(l)
+    return (expected_sum - actual_sum)
+
+print(missing_no(l))
+
+"""
+def find_missing(arr,num):
+    arr = []
+    num = int(input("Enter the lenght of the array"))
+    for int in range(num-1):
+        m = int(input("Enter the element/number"))
+        arr.append(m)
+
+    missing_num = find_missing(arr,num)
+    print(f"The missing num is {missing_num}")
+"""
+
+def find_missing_no(l):
+    for i in l:
+        if i<i+1:
+            pass
+        else:
+            break
+    return i
+l1= [1,2,3,5,6,7]
+print(missing_no(l1))
+
