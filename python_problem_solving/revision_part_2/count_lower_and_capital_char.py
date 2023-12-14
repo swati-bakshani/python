cb17d245a67308cbbfe06a2fd6a9cc3b3b93b237
swati <swati.bakshani@gmail.com>
2023-12-14 10:28:25 +0530

    count of letters in lower and upper case

---
+++ python_problem_solving/revision_part_2/count_lower_and_capital_char.py
@@ -0,0 +1,11 @@
+def count_char(input_str):
+    count_lower,count_upper = 0,0
+    for char in input_str:
+        if (char.islower()):
+            count_lower = count_lower+1
+
+        elif (char.isupper()):
+            count_upper = count_upper+1
+    return count_lower,count_upper
+
+print(count_char("hello GReekCOders"))
