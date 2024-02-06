72ddcde973c1b94c877facd5e651ff439a764a24
swati <swati.bakshani@gmail.com>
2024-02-06 23:02:24 +0530

    add string in the given string

---
+++ python_problem_solving/revision_part_2/insert_str_in_middle_of_given_str.py
@@ -0,0 +1,7 @@
+given_input = "geekcodders in raipur"
+# add = the city
+
+def add_str(input_str):
+    return input_str.replace("in", "in the city")
+
+print(add_str(given_input))
---
+++ python_problem_solving/revision_part_4/insert_string_in_given_string.py
@@ -0,0 +1,6 @@
+given_input = "geekcodders in raipur"
+
+def add_string(given_input):
+    return given_input.replace("in","in the city of")
+
+print(add_string(given_input))
