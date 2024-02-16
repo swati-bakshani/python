3af311d94cd47cb1feb014f263dea2731f5c1cc3
swati <swati.bakshani@gmail.com>
2024-02-16 09:51:16 +0530

    swapping of num with user input

---
+++ python_problem_solving/revision_part_4/swaping_num.py
@@ -0,0 +1,14 @@
+
+def swap_num(a,b):
+    a,b = b,a
+    return a,b
+
+print(swap_num(4,3))
+
+def swap_num(a,b):
+    a,b = b,a
+    return a,b
+
+input1 = input("Enter your first value:")
+input2 = input("Enter your second value:")
+print(swap_num(input1,input2))
