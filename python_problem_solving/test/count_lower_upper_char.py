7f36a0e1ebb3d706d35116d0734d70d9c4d04047
swati <swati.bakshani@gmail.com>
2024-04-04 23:35:18 +0530

    count lower and upper char of the given input

--- python_problem_solving/test/count_lower_upper_char.py
+++ python_problem_solving/test/count_lower_upper_char.py
@@ -1,4 +1,16 @@
-input_str= " Nikhil Is a GooD Boy"
+# input_str= " Nikhil Is a GooD Boy"
+#
+# def count_upper_lower_char(input_str):
+#     upper,lower = (0,0)
+#
+#     for char in input_str:
+#         if (char.islower()):
+#             lower = lower+1
+#         elif (char.isupper()):
+#             upper = upper+1
+#     return (upper,lower)
+#
+# print(count_upper_lower_char(input_str))
 
 def count_upper_lower_char(input_str):
     upper,lower = (0,0)
@@ -10,21 +22,9 @@ def count_upper_lower_char(input_str):
             upper = upper+1
     return (upper,lower)
 
+input_str = input("Enter your Statement:" )
+
 print(count_upper_lower_char(input_str))
-
-def count_upper_lower_char(input):
-    upper,lower = (0,0)
-
-    for char in input_str:
-        if (char.islower()):
-            lower = lower+1
-        elif (char.isupper()):
-            upper = upper+1
-    return (upper,lower)
-
-input = input("Enter your Statement:" )
-
-# print(count_upper_lower_char(input_str))
 upper, lower = count_upper_lower_char(input_str)
 print(f"count of Lower Char is {lower} and upper char is {upper}")
 
