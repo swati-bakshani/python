6894e51192e3d8c5e69b0c94390c2ebdc996f05a
swati <swati.bakshani@gmail.com>
2024-04-15 10:11:12 +0530

    insert str in a given string

---
+++ python_problem_solving/test/insert_str_in_given_string.py
@@ -0,0 +1,8 @@
+input_str = "swati is girl"
+add = "a good"
+
+# output = swati is a good girl
+def adding_str(input_str):
+    return input_str.replace("is","is a good")
+
+print(adding_str(input_str))
