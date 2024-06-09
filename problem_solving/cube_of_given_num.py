5ccd8e8de108494dad3857a7aeb8a6ed26d2e7c3
swati <swati.bakshani@gmail.com>
2024-06-09 10:23:18 +0530

    cobe of given numbers in list and also print cube of only odd numbers in given list

---
+++ problem_solving/cube_of_given_num.py
@@ -0,0 +1,24 @@
+input_list = [1,2,3,4,5,6]
+
+def Cube_of_num(l):
+    output_list = []
+    for num in l:
+        cube = num*num*num
+        output_list.append(cube)
+    return output_list
+
+print(Cube_of_num(input_list))
+
+def Cube_of_num(l):
+    output_list = []
+    for num in l:
+        if num%2 == 1:
+            cube = num*num*num
+            output_list.append(cube)
+    return output_list
+
+print(Cube_of_num(input_list))
+
+
+
+
