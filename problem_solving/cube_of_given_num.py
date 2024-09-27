2e5624cd9970ad21033dcea0056797b4f54ddcfa
swati <swati.bakshani@gmail.com>
2024-09-27 09:50:05 +0530

    cube off list, odd numbers and even numbers in list

--- problem_solving/cube_of_given_num.py
+++ problem_solving/cube_of_given_num.py
@@ -9,10 +9,22 @@ def Cube_of_num(l):
 
 print(Cube_of_num(input_list))
 
+print('......cube of odd numbers......')
 def Cube_of_num(l):
     output_list = []
     for num in l:
         if num%2 == 1:
+            cube = num*num*num
+            output_list.append(cube)
+    return output_list
+
+print(Cube_of_num(input_list))
+
+print('......cube of even numbers.....')
+def Cube_of_num(l):
+    output_list = []
+    for num in l:
+        if num%2 == 0:
             cube = num*num*num
             output_list.append(cube)
     return output_list
